ac9a8805e399123df2cd35a2177d8fafa02d28ac
Gabriel Oliveira <ateliergabdias@gmail.com>
2023-04-19 19:16:11 -0300

    update header with mockup phone and shipping message inside className col-6, booya

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <>
+      <header className="header-top-strip py-3">
+        <div className="container-xxl">
+          <div className="row"> 
+            <div className="col-6">
+              <p>Free Shipping over $150</p>
+            </div>
+            <div className="col-6">
+              <p>
+                Hotline: <a href="tel:+55 11 999990000">+55 11 99999-0000</a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
   );
 };
 
