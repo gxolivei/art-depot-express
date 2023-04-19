87b85d2b6dff43a12f2dded20e16fd11a0f8eb0f
Gabriel Oliveira <ateliergabdias@gmail.com>
2023-04-19 19:39:01 -0300

    Future ref. to self: take pauses. Drink water. Header part 1

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,11 +8,12 @@ const Header = () => {
         <div className="container-xxl">
           <div className="row"> 
             <div className="col-6">
-              <p>Free Shipping over $150</p>
+              <p className="text-white mb-0">Free Shipping over $150</p>
             </div>
             <div className="col-6">
-              <p>
-                Hotline: <a href="tel:+55 11 999990000">+55 11 99999-0000</a>
+              <p className="text-end text-white mb-0"> 
+                Hotline:
+                <a className="text-white" href="tel:+55 11 999990000">+55 11 99999-0000</a>
               </p>
             </div>
           </div>
