973712ea8610a96f2ed3b927adaf4b613247f419
Gabriel Oliveira <ateliergabdias@gmail.com>
2023-04-23 13:25:39 -0300

    wishlist.svg and changes in color schemes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { NavLink, Link } from 'react-router-dom';
 import { SlMagnifier } from 'react-icons/sl';
-import { SlRefresh } from 'react-icons/sl';
 
 const Header = () => {
   return (
@@ -46,27 +45,36 @@ const Header = () => {
             <div className="col-5">
               <div className="header-upper-links d-flex align-items-center justify-content-between">
                 <div>
-                  <Link>
-                    <img src="images/compare.svg" alt="" />
-                    <p></p>
+                  <Link className="d-flex align-items-center gap-10 text-white">
+                    <img src="images/compare.svg" alt="compare" />
+                    <p className="mb-0">
+                      Compare <br /> Products
+                      </p>
                   </Link>
                 </div>
                 <div>
-                  <Link>
-                    <img src="" alt="" />
-                    <p></p>
+                  <Link className="d-flex align-items-center gap-10 text-white">
+                  <img src="images/wishlist.svg" alt="wishlist" />
+                    <p className="mb-0">
+                      Favourite <br /> Wishlist
+                    </p>
                   </Link>
                 </div>
                 <div>
-                  <Link>
-                    <img src="" alt="" />
-                    <p></p>
+                  <Link className="d-flex align-items-center gap-10 text-white">
+                    <img src="images/user.svg" alt="user" />
+                    <p className="mb-0">
+                      Log in <br /> My Account
+                    </p>
                   </Link>
                 </div>
                 <div>
-                  <Link>
-                    <img src="" alt="" />
-                    <p></p>
+                  <Link className="d-flex align-items-center gap-10 text-white">
+                    <img src="images/cart.svg" alt="cart" />
+                    <div className="d-flex flex-column gap-10">
+                      <span className="badge bg-white text-dark">0</span>
+                      <p className="mb-0">$ 500</p>
+                    </div>
                   </Link>
                 </div>
               </div>
