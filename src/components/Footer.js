e88e9840cbac977c19e0691c5ae175478f9e9ffc
Gabriel Oliveira <ateliergabdias@gmail.com>
2023-04-23 17:48:54 -0300

    newsletter animation

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,7 +11,7 @@ const Footer = () => {
           <div className="row align-items-center">
             <div className="col-5">
               <div className="footer-top-data d-flex gap-30 align-items-center">
-                <img src="images/newsletter.png" alt="newsletter" />
+                <img src="images/newsletter.png" alt="newsletter" className="newsletter-image" />
                 <h2 className="mb-0 text-white">Sign Up for Newsletter</h2>
               </div>
             </div>
