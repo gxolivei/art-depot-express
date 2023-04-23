e56701e5ae370c98c41813b8fd9b931b8855c185
Gabriel Oliveira <ateliergabdias@gmail.com>
2023-04-23 15:00:13 -0300

    add header-bottom codes

--- src/components/Header.js
+++ src/components/Header.js
@@ -82,6 +82,50 @@ const Header = () => {
           </div>
         </div>
       </header>
+      <header className="header-bottom py-3">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <div className="menu-bottom d-flex align-items-center gap-30">
+                <div>
+                  <div className="dropdown">
+                    <button class="btn btn-secondary dropdown-toggle bg-transparent border-0 gap-15 d-flex align-items-center" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                      <span className="me-5 d-inline-block">
+                        Shop Categories
+                      </span>
+                    </button>
+                    <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                      <li>
+                        <Link className="dropdown-item text-white" to="">
+                          Action
+                        </Link>
+                      </li>
+                      <li>
+                        <Link className="dropdown-item text-white" to="">
+                          Another action
+                        </Link>
+                      </li>
+                      <li>
+                        <Link className="dropdown-item text-white" to="">
+                          Something else here
+                        </Link>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <dir className="menu-links">
+                  <div className="d-flex align-items-center gap-15">
+                      <NavLink to="/">Home</NavLink>
+                      <NavLink to="/">Our Store</NavLink>
+                    <NavLink to="/">Blogs</NavLink>
+                    <NavLink to="/contact">Contact</NavLink>
+                  </div>
+                </dir>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
     </>
   );
 };
